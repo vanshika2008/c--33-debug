469e2fb7082a97be4cab899b67f146cae83dd439
vanshika2008 <jainvanshika2008@gmail.com>
2020-09-14 19:59:49 +0530

    debug

--- sketch.js
+++ sketch.js
@@ -81,9 +81,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -94,6 +94,9 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
+        gameState = "onSling";
+       bird.trajectory = [];
+       Matter.Body.setPosition(bird.body,{x:200, y: 50});
        slingshot.attach(bird.body);
     }
 }
